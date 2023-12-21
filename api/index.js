351349a7b538ab054c5d1019e5fdd422f7638932
Ian Pelina <ianpelina42@gmail.com>
2023-12-21 15:02:33 +0100

    initialisation repo backend

---
+++ api/index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const app = require('express')();
+const cors = require('cors');
+
+
+const works = require("./data/works");
+
+const sendMail = require("./services/nodemailer");
+
+// Autorisation CORS
+app.use(cors({
+  origin: process.env.CLIENT,  // Ajoutez l'origine de votre frontend ici
+  credentials: true,
+}));
+
+app.use(require('express').json())
+
+app.get("/api/works", (req, res) => {
+  res.status(200).json({ works });
+})
+
+app.get("/api/works/:id", (req, res) => {
+  const { id } = req.params;
+  const work = works.find((work) => work.id === id);
+  res.status(200).json({ work });
+})
+
+app.post("/api/mail", async (req, res) => {
+  const { name, surname, email, area } = req.body;
+  await sendMail({
+    from: email,
+    to: process.env.EMAIL_ADDRESS,
+    subject: `Prise de contact (${email})`,
+    html: `
+      <h1>${name} ${surname} souhaite prendre contact avec vous</h1>
+      <h2>Message reçu de sa part</h2>
+      <p>${area}</p>
+    `
+  });
+  res.status(200).json({ message: 'Message envoyé' })
+})
+
+module.exports = app;
---
+++ api/services/nodemailer.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+console.log(process.env.MAIL_AUTH_PASSWORD);
+
+const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_AUTH_USER,
+      pass: process.env.MAIL_AUTH_PASSWORD
+    }
+});
+
+
+function sendMail(options) {
+    transport.sendMail(options);
+}
+
+module.exports = sendMail;
